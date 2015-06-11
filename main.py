9d4703628672fa3654de6c0295b2daea49ce964d
Brian Díaz <briandiaz@outlook.com>
2015-06-10 23:51:08 -0400

    Code refactor. Add class Classifier, Smile and upgrade FaceDetector to Detector class. Add Smile detector functionality

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import cv2
 from image import Image 
-from facedetector import FaceDetector 
+from detector import Detector
+from classifier import Classifier
+import time
 
+video_capture = cv2.VideoCapture(0) # Create instance of videoCapture on Default WebCamera
 
-images = ["images/1.jpg", "images/2.jpg", "images/3.jpg", "images/4.jpg"]
+haarcascade_classifier = Classifier("alt", "eye_glasses", "smile") # Create instance of Classifier with haarcascade classifier xml preferred.
 
-def detect_faces():
-	for img_path in images:
-		img = Image(img_path, 1).load()
-		detector = FaceDetector(img, 1.1, 5, (30, 30), cv2.cv.CV_HAAR_SCALE_IMAGE, "alt", "eye_glasses")
-		detector.detect()
-		cv2.imshow(img_path, img)
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
+video_capture.open(0) # Open Default WebCamera
+time.sleep(5) # Wait Camera to respond
 
-if __name__ == '__main__':
-	detect_faces()
+while True: 
+	ret, image = video_capture.read()
+	detector = Detector(image, 1.1, 5, (30, 30), cv2.cv.CV_HAAR_SCALE_IMAGE, haarcascade_classifier)
+	detector.detect()
+	cv2.imshow("Video", image)
+
+	key = cv2.waitKey(30) & 0xff
+	if key == 27: # If Escape key is pressed video-capture ends.
+		break
+
+video_capture.release()
+cv2.destroyAllWindows()
