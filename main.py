1a1edccbef4561f40f381c7f87d7dbde89d2f1b3
Brian Díaz <brianediaz7@gmail.com>
2021-09-27 19:06:48 -0400

    Fix: Fix cv2.imshow with newer OpenCV versions.

--- main.py
+++ main.py
@@ -12,10 +12,13 @@ video_capture.open(0) # Open Default WebCamera
 time.sleep(5) # Wait Camera to respond
 
 while True: 
-	ret, image = video_capture.read()
-	detector = Detector(image, 1.1, 5, (30, 30), cv2.DIST_L2, haarcascade_classifier)
-	detector.detect()
-	cv2.imshow("Video", image)
+	is_read, image = video_capture.read()
+	if is_read:
+		detector = Detector(image, 1.1, 5, (30, 30), cv2.DIST_L2, haarcascade_classifier)
+		detector.detect()
+		cv2.imshow('video', image)
+	else:
+		cv2.waitKey(1000)
 
 	key = cv2.waitKey(30) & 0xff
 	if key == 27: # If Escape key is pressed video-capture ends.
